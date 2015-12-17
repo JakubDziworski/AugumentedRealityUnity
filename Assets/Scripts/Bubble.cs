a02a4140f97fc219a0dc868158c447b660cd51ea
jakubdziworski <jakub.dziworski@gmail.com>
2015-12-17 18:55:14 +0100

    animations fix

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -9,11 +9,12 @@ public class Bubble : MonoBehaviour
     private CanvasGroup canvasGroup;
     private const float animDuration = 0.5f;
     private Vector3 defaultScale;
+
 	// Use this for initialization
 	void Awake () 
     {
 	    canvasGroup = GetComponent<CanvasGroup>();
-        HOTween.defEaseType = EaseType.EaseInOutExpo;
+            HOTween.defEaseType = EaseType.EaseInOutExpo;
 	    defaultScale = canvasGroup.transform.localScale;
     }
 	
@@ -25,13 +26,21 @@ public class Bubble : MonoBehaviour
 
     public void Show()
     {
+        StopAnimations();
         HOTween.To(canvasGroup, 0.1f, "alpha", 1f, false).Play();
         HOTween.To(canvasGroup.transform, animDuration, "localScale", defaultScale, false, EaseType.EaseOutBack, 0).Play();
     }
 
     public void Hide()
     {
+        StopAnimations();
         HOTween.To(canvasGroup, animDuration / 2.0f, "alpha", 0f, false, EaseType.EaseInOutExpo, animDuration - 0.1f).Play();
         HOTween.To(canvasGroup.transform, animDuration, "localScale", new Vector3(), false, EaseType.EaseInBack, 0).Play();
+    }
+
+    private void StopAnimations()
+    {
+        HOTween.Complete(canvasGroup);
+        HOTween.Complete(canvasGroup.transform);
     }
 }
