33e0b13329ad19f136ebfd2cc597f44fd99f36ec
jakubdziworski <jakub.dziworski@gmail.com>
2016-06-15 19:02:40 +0200

    ukryty przycisk konfiguracja

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class GUIManager : MonoBehaviour {
+
+	// Use this for initialization
+
+    private GameObject adjustDialog;
+
+	void Start () 
+    {
+        adjustDialog = GameObject.Find("adjustDialog");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && Input.GetKeyDown(KeyCode.K))
+        {
+            adjustDialog.SetActive(true);
+        }
+	}
+}
